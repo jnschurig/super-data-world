daa51959ab4e5bd314f4ccf42496910098cf7f0e
jschurig <jnschurig@gmail.com>
2021-03-03 17:21:42 -0700

    Minor improvements to gacha. Put the constants in a constants file.

--- public/python/gachaball.py
+++ public/python/gachaball.py
@@ -1,5 +1,5 @@
 import random, re
-import world_events
+import world_events, constants
 import urllib.request
 
 # Check for gacha list existence.
@@ -19,111 +19,11 @@ def generate_prizes(count):
         'Your lucky color is {color}',
         'Today you will have {luck} luck'
     ]
+    sayings = constants.GACHA_SAYINGS
+    colors = constants.GACHA_COLORS
+    luck = constants.GACHA_LUCK
 
-    sayings = [
-        'Man who run in front of car get tired. Man who run behind car get exhausted',
-        'Scientists thought light traveled fastest of all. Then they realized bad news travels faster still',
-        'In a closed mouth, flies do not enter',
-        'If thine enemy wrong thee, buy each of his children a drum',
-        'You are unique, just like everyone else',
-        "Anything is possible if you don't know what you are talking about.",
-        'Fart silently, then ask "Do you smell popcorn?"',
-        'Never assume malice when ignorance is just as likely',
-        'Two wrongs is a great start',
-        'Never forget to top off your blinker fluid',
-        'All the world is mad save for me and thee, and sometimes I wonder about thee',
-        'An apple a day keeps the doctor away, which is good because doctors want your blood',
-        "If at first you don’t succeed, that shounds about right",
-        'If it can happen, it will',
-        'Life is too short to eat pizza crust',
-        'The face of a child can say it all, especially the mouth part of the face',
-        'All glory to the Hypno Toad',
-        "A person at the zoo is staring sadly at a baguette in a cage. A zookeeper comes by and tells them not to worry because it's bread in captivity",
-        'A dog is a great companion, but might be troublesome if they stick their head out the window while re-entering Earth\'s atmosphere',
-        "Don't mind me, I'm just a robot slave who does nothing but serve out meaningless prizes",
-        "If you must choose between two evils, pick the one you've never tried before",
-        'Buy a plunger before you need it',
-        "If at first you don't succeed, fry fry a hen",
-        'The only thing worse than a cold toilet seat is a warm one',
-        'Imagine you, meeting me here',
-        'Money can be exchanged for goods and services. This is something else',
-        'Salmon. Am I right or what?',
-        'Some fun, eh kid?',
-        'An eggplant is neither an egg, nor a plant',
-        'You can do anything with gacha',
-        # 'Welcome to zombocom',
-        'Never spit into the wind',
-        "Don't start none, won't be none",
-        "One man's trash is another man's last minute Christmas gift",
-        "My best advice isn't good enough"
-    ]
-
-    colors = [
-        'blue',
-        '7',
-        'candy',
-        'some kind of... orange?',
-        'red',
-        'yellow',
-        'pink',
-        'green',
-        'purple',
-        'blue',
-        'puce',
-        'nice',
-        'not lucky',
-        'ok I guess',
-        'white',
-        'not white',
-        'off-gray',
-        'off-grey',
-        'deep forest green',
-        'bread',
-        'fuchsia',
-        'wrong',
-        'troubling',
-        'nasty green',
-        'purplish grey',
-        'red-pink',
-        'dark mustard',
-        'bordeaux',
-        'taupe',
-        'burnt, leathery bacon',
-        'bright mauve',
-        'minty orange',
-        'slightly mossy',
-        'sweet and sour',
-        'claret',
-        'mud'
-    ]
-
-    luck = [
-        '"lucky"',
-        'terrible',
-        'good',
-        'bad',
-        'great',
-        'a moderate amount of',
-        'duck',
-        'sideways',
-        'extra terrestrial',
-        'some kind of',
-        '1-in-20',
-        'pants-on-fire',
-        'leper',
-        'leprechaun',
-        'artisanal',
-        'bodily',
-        'random',
-        'randomizer',
-        'ambiguous',
-        'perfect',
-        'profound',
-        'craptastic',
-        'inconvenient'
-    ]
-
-    # Savinv this for a rainy day. It totally works, but I don't think we need that for now.
+    # Saving this for a rainy day. It totally works, but I don't think we need that for now.
     # color_url = 'https://xkcd.com/color/rgb.txt'
     # contents = urllib.request.urlopen(color_url).read()
     # contents = str(contents)
